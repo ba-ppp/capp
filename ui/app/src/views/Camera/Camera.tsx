eff65f7334888aa1123ca90d9476903b40b30871
Anh Khôi Lê <leanhkhoi2611@gmail.com>
2022-07-06 17:04:18 +0700

    create tab-navigator

---
+++ ui/app/src/views/Camera/Camera.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Camera = () => {
+  return (
+    <View>
+      <Text>Camera</Text>
+    </View>
+  );
+};
+
+export default Camera;
