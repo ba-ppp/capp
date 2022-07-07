ae0d11560ef8f91a0175d0e0f2cbecf0e7f3f38b
Anh Khôi Lê <leanhkhoi2611@gmail.com>
2022-07-07 23:36:29 +0700

    add icon

--- ui/app/App.tsx
+++ ui/app/App.tsx
@@ -13,12 +13,35 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Home from './src/views/Home/Home';
 import Camera from './src/views/Camera/Camera';
+import {Image} from 'react-native';
 
 const Tab = createBottomTabNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: () => {
+            let iconSize = 30;
+            if (route.name === 'Home') {
+              return (
+                <Image
+                  style={{width: iconSize, height: iconSize}}
+                  source={require('./assets/home_icon.png')}
+                />
+              );
+            } else if (route.name === 'Camera') {
+              return (
+                <Image
+                  style={{width: iconSize, height: iconSize}}
+                  source={require('./assets/camera_icon.png')}
+                />
+              );
+            }
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}>
         <Tab.Screen name="Home" component={Home} />
         <Tab.Screen name="Camera" component={Camera} />
       </Tab.Navigator>
