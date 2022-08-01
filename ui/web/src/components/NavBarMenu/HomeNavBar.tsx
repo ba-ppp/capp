9b2a3da9ddb6cc0d3ff39f624728d63229f86951
bap <phuongtr3112@gmail.com>
2022-08-01 20:55:57 +0700

    FE: Responsive for homepage

--- ui/web/src/components/NavBarMenu/HomeNavBar.tsx
+++ ui/web/src/components/NavBarMenu/HomeNavBar.tsx
@@ -6,7 +6,7 @@ import { ReactComponent as DropIcon } from "asset/icons/arrow_drop_down.svg";
 
 export const HomeNavBar = () => {
   return (
-    <div className="bg-white h-[136px] z-20 shadow-nav fixed left-0 right-0 top-0">
+    <div className="hidden lg:block bg-white h-[136px] z-20 shadow-nav fixed left-0 right-0 top-0">
       <div
         className="flex items-center justify-between h-full m-auto"
         tw="width[80%]"
