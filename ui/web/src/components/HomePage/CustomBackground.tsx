adaab339ce68ea507ac37d84938b1f36c36ac427
Veo CT <votrieuvy1661@gmail.com>
2022-08-04 17:27:47 +0700

    add upload page

--- ui/web/src/components/HomePage/CustomBackground.tsx
+++ ui/web/src/components/HomePage/CustomBackground.tsx
@@ -3,80 +3,80 @@ import { loadFull } from "tsparticles";
 import { Engine } from "tsparticles-engine";
 
 export const CustomBackground = () => {
-  const particlesInit = async (main: Engine) => {
-    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
-    await loadFull(main);
-  };
+    const particlesInit = async (main: Engine) => {
+        // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        await loadFull(main);
+    };
 
-  return (
-    <div className="absolute-full">
-      <Particles
-        id="tsparticles"
-        init={particlesInit}
-        options={{
-          background: {
-            color: {
-              value: "#fff",
-            },
-          },
-          fpsLimit: 120,
-          interactivity: {
-            modes: {
-              push: {
-                quantity: 4,
-              },
-              repulse: {
-                distance: 200,
-                duration: 0.4,
-              },
-            },
-          },
-          particles: {
-            color: {
-              value: "#0a0a39",
-            },
-            links: {
-              color: "#991cca",
-              distance: 150,
-              enable: true,
-              opacity: 0.5,
-              width: 1,
-            },
-            collisions: {
-              enable: true,
-            },
-            move: {
-              direction: "none",
-              enable: true,
-              outModes: {
-                default: "bounce",
-              },
-              random: false,
-              speed: 3,
-              straight: false,
-            },
-            number: {
-              density: {
-                enable: true,
-                area: 1200,
-              },
-              value: 50,
-            },
-            opacity: {
-              value: 0.5,
-            },
-            shape: {
-              type: "circle",
-            },
-            size: {
-              value: { min: 1, max: 3 },
-            },
-          },
-          detectRetina: true,
-        }}
-      />
-    </div>
-  );
+    return (
+        <div className="absolute-full">
+            <Particles
+                id="tsparticles"
+                init={particlesInit}
+                options={{
+                    background: {
+                        color: {
+                            value: "#fff",
+                        },
+                    },
+                    fpsLimit: 120,
+                    interactivity: {
+                        modes: {
+                            push: {
+                                quantity: 4,
+                            },
+                            repulse: {
+                                distance: 200,
+                                duration: 0.4,
+                            },
+                        },
+                    },
+                    particles: {
+                        color: {
+                            value: "#0a0a39",
+                        },
+                        links: {
+                            color: "#991cca",
+                            distance: 150,
+                            enable: true,
+                            opacity: 0.5,
+                            width: 1,
+                        },
+                        collisions: {
+                            enable: true,
+                        },
+                        move: {
+                            direction: "none",
+                            enable: true,
+                            outModes: {
+                                default: "bounce",
+                            },
+                            random: false,
+                            speed: 3,
+                            straight: false,
+                        },
+                        number: {
+                            density: {
+                                enable: true,
+                                area: 1200,
+                            },
+                            value: 50,
+                        },
+                        opacity: {
+                            value: 0.5,
+                        },
+                        shape: {
+                            type: "circle",
+                        },
+                        size: {
+                            value: { min: 1, max: 3 },
+                        },
+                    },
+                    detectRetina: true,
+                }}
+            />
+        </div>
+    );
 };
---
+++ ui/web/src/components/UploadPage/UploadPage.tsx
@@ -0,0 +1,30 @@
+import { RootState } from "app/store/store";
+import { ReactComponent as UploadIcon } from "asset/icons/upload-icon.svg";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+
+export default function UploadPage() {
+    return (
+        <div className="pt-36 h-screen min-h-[800px] flex flex-col items-center justify-center">
+            <div className="w-[1270px] p-6 min-h-[640px] bg-bg-200 text-bg-800 rounded-lg">
+                <label className="bg-bg-400 flex flex-col items-center w-full h-[592px] text-4xl transition border border-bg-800 border-dashed rounded-lg appearance-none cursor-pointer hover:border-bg-600">
+                    <span className="mt-28">
+                        <UploadIcon width={70} />
+                    </span>
+                    <span className="font-medium mt-28">
+                        Drop files here or{" "}
+                        <span className="font-extrabold">click to upload </span>
+                    </span>
+                    <input
+                        type="file"
+                        name="file_upload"
+                        className="hidden"
+                        accept="image/*"
+                        multiple={true}
+                    />
+                </label>
+            </div>
+        </div>
+    );
+}
